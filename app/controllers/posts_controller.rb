a69c052c52993b39bc7d7db1800157b4ce9bf9c0
shaoyu <571398414@qq.com>
2017-07-02 19:09:36 +0800

    新增和删除

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,27 @@
 class PostsController < ApplicationController
-
+  before_action :authenticate_user!, :only => [:create, :destroy]
   def index
     @posts = Post.all.order('id DESC')
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    @post.save
+
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
